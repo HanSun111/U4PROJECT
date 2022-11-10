c478e9d432c48cb434334831c59d5d44b27a1d31
BT_4N2_13 <hsun7920@bths.edu>
2022-11-10 12:33:20 -0500

    dialogue

--- src/YourFateRunner.java
+++ src/YourFateRunner.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class YourFateRunner {
     public static void main(String[] args) {
         Scanner mamaOHOHOHHHHH = new Scanner(System.in);
-        System.out.println("");
+        System.out.println("Welcome to Survive Mom Simulator. input a character name: ");
 
 
 
